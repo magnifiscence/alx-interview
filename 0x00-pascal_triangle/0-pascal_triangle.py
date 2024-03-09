cb5b64f9d0aa32bc17eb5ef413fc747e2142bb05
tata <tdivine284@gmail.com>
2024-03-09 20:24:27 +0000

    task done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal's triangle
+    of n
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        last_row = triangle[-1]
+        for j in range(len(last_row)-1):
+            row.append(last_row[j] + last_row[j+1])
+        row.append(1)
+        triangle.append(row)
+    return triangle
